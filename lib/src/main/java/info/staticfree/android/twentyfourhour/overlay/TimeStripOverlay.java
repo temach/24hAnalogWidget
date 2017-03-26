6ee6e34ecc8d6dc161047fedf4706926e6fcf6be
Artem Abramov <tematibr@gmail.com>
2017-03-26 22:51:07 +0300

    Added TimeStripOverlay

---
+++ lib/src/main/java/info/staticfree/android/twentyfourhour/overlay/TimeStripOverlay.java
@@ -0,0 +1,15 @@
+package info.staticfree.android.twentyfourhour.overlay;
+
+import java.util.Calendar;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by artem on 3/26/17.
+ */
+
+public class TimeStripOverlay implements DialOverlay {
+
+}
